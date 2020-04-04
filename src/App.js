da6a885954428568c681760beabb4e8ada61d19f
Shinji Obana <willshinji@gmail.com>
2020-04-04 17:31:13 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Auth } from "aws-amplify";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    username: '',
+    password: '',
+    bucket: '',
+    user: {},
+    error: {
+      message: null,
+      code: null,
+      name: null
+    },
+    isUserLogged: false
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.id]: event.target.value })
+  }
+
+  signIn = async event => {
+    event.preventDefault()
+
+    try {
+      const { username, password } = this.state
+      const user = await Auth.signIn(username, password)
+      const credentials = await Auth.currentCredentials()
+      console.log(credentials)
+      this.setState({ ...this.state, isUserLogged: true, user: user.attributes })
+      console.log(user)
+    } catch (error) {
+      this.setState({...this.state, error})
+    }
+
+  }
+
+  listObjects = async () => {
+    // const s3 = new aws.S3()
+    const { bucket: Bucket } = this.state
+    var params = {
+      Bucket
+    };
+    console.log(params)
+
+
+    // s3.listObjects(params, function (err, data) {
+      // if (err) logMessage(err.message);
+      // else {
+      //   data.Contents.forEach(element => {
+      //     console.log(element.Key);
+      //   });
+
+      // }
+
+
+    // })
+  }
+
+  render() {
+    const { handleChange, signIn, listObjects } = this
+    const { username, password, isUserLogged, bucket } = this.state
+    return (
+      <div className="App">
+        <header className="App-header">
+          {!isUserLogged ?
+            (
+              <form onSubmit={signIn}>
+                <input id='username' onChange={handleChange} value={username}></input>
+                <input id='password' type='password' onChange={handleChange} value={password}></input>
+                <button>Sign in</button>
+                <div>
+                  {this.state.error.message}
+                </div>
+              </form>
+            ) :
+            (
+              <>
+                <p>User Logged {this.state.user.email}</p>
+                <input id='bucket' value={bucket} onChange={handleChange} />
+                <button onClick={listObjects}>List Files in S3</button>
+              </>
+            )}
+        </header>
+      </div>
+    );
+  }
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Amplify from 'aws-amplify'
+import config from './config/aws-config.json'
+
+Amplify.configure({
+  region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID 
+})
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +20,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();

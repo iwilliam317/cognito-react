70ed67101cebb1f0193e8311ec8ec7027276c569
Shinji Obana <willshinji@gmail.com>
2020-06-07 15:51:17 +0100

    Updates

---
+++ precache-manifest.784510a2d7effaeacdfdcf943c27499e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3db04369ee6049b143c3998640f578f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a350224e558cc2505141",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b6b2e265ca9fd7b01649",
+    "url": "/static/js/2.48fc3c42.chunk.js"
+  },
+  {
+    "revision": "e04852c4130931704ebcd1e8ffdcd8c4",
+    "url": "/static/js/2.48fc3c42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a350224e558cc2505141",
+    "url": "/static/js/main.f5b8c45a.chunk.js"
+  },
+  {
+    "revision": "20bfe36f8a8c236d1ad0",
+    "url": "/static/js/runtime-main.52148756.js"
+  }
+]);
